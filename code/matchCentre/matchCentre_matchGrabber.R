fd304f209134aa5265d3ecaddbbad5b9f17d27da
Aaron Fox <aaron.f@deakin.edu.au>
2023-06-20 13:50:48 +1000

    SSN rd 14 2023 data.

--- code/matchCentre/matchCentre_matchGrabber.R
+++ code/matchCentre/matchCentre_matchGrabber.R
@@ -92,7 +92,7 @@ compLeague <- "SSN"
 compId <- 12045
 
 #Set the round to get
-getRound <- 13
+getRound <- 14
 
 #Loop through matches (set to irresponsibly large value but breaks used)
 for (getMatch in 1:25) {
